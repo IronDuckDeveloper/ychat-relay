59b19a6b9faa01fcba911f685b27688862a10793
Egor Lachimov <lachimov@lifehack.studio>
2026-07-11 20:31:22 +0700

    Docker compose

--- deploy.example.sh
+++ deploy.example.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Папки, которые МЫ ИГНОРИРУЕМ (аналог секции ignore в SFTP)
-EXCLUDES="--exclude=node_modules --exclude=data --exclude=.git --exclude=.vscode --exclude=deploy.sh --exclude=deploy.example.sh --exclude=tests"
+# Папки и файлы, которые МЫ ИГНОРИРУЕМ (аналог секции ignore в SFTP)
+EXCLUDES="--exclude=node_modules --exclude=data --exclude=.git --exclude=.vscode --exclude=deploy.sh --exclude=deploy.example.sh --exclude=tests --exclude=.env --exclude=.env.example.sh"
 
 echo "🚀 Начинаем деплой на кластер ychat..."
 
-# 1. Загрузка на Сервер А
+# 1. Загрузка и перезапуск на Сервер А
 echo "📦 Отправка на Сервер А (X.X.X.X)..."
 rsync -avz $EXCLUDES ./ root@X.X.X.X:/root/ychat-relay/
+echo "🔄 Перезапуск Docker на Сервере А..."
+ssh root@X.X.X.X "cd /root/ychat-relay && docker-compose up -d --build"
 
-# 2. Загрузка на Сервер Б
+# 2. Загрузка и перезапуск на Сервер Б
 echo "📦 Отправка на Сервер Б (X.X.X.X)..."
 rsync -avz $EXCLUDES ./ root@X.X.X.X:/root/ychat-relay/
+echo "🔄 Перезапуск Docker на Сервере Б..."
+ssh root@X.X.X.X "cd /root/ychat-relay && docker-compose up -d --build"
 
-echo "✅ Все файлы успешно обновлены на обоих серверах!"
+echo "✅ Все файлы успешно обновлены, а контейнеры перезапущены на обоих серверах!"
